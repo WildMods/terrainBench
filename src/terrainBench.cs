5335dc61a2ca3f105ff76fbdf8f2a7a797b44367
torph <73564623+Torphedo@users.noreply.github.com>
2026-07-15 01:09:21 -0400

    Working oead decompression

--- src/terrainBench.cs
+++ src/terrainBench.cs
@@ -1,7 +1,24 @@
 
 public static class TerrainBench {
 
-    public static void Main() {
-        Console.WriteLine("hello");
+    public static void Main(string[] args) {
+        foreach (string s in args) {
+            try {
+                Console.WriteLine("Decompressing {0}", s);
+                byte[]? buf = null;
+                CsOead.Yaz0.TryDecompress(s, out buf);
+                if (buf == null) {
+                    Console.WriteLine("Failed to decompress.");
+                    continue;
+                }
+
+                string outPath = s + ".dec";
+                using (var f = new FileStream(outPath, FileMode.Create, FileAccess.Write)) {
+                    f.Write(buf, 0, buf.Length);
+                }
+            } catch (System.Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
