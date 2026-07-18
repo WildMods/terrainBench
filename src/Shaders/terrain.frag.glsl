3c9d9a7c417506ed2ed3da6cc308554ef8fc358a
torph <73564623+Torphedo@users.noreply.github.com>
2026-07-18 10:00:26 -0400

    Remove hardcoded solid colors in fragment shader

--- src/Shaders/terrain.frag.glsl
+++ src/Shaders/terrain.frag.glsl
@@ -6,31 +6,6 @@ out vec4 finalColor;
 layout (binding = 1) uniform sampler2D matTex;
 layout (binding = 2) uniform sampler2DArray colorTextures;
 
-// https://zeldamods.org/wiki/MATE
-const vec3 green = vec3(0, 1, 0);
-const vec3 darkGreen = vec3(0, 1, 0);
-const vec3 grey = vec3(0.5);
-const vec3 lightGrey = vec3(0.8);
-const vec3 redRock = vec3(0.8, 0, 0);
-const vec3 orangeRock = vec3(1, 0.6, 0.25);
-const vec3 blueRock = vec3(0.5, 0.5, 0.8);
-const vec3 soil = vec3(0.45, 0.35, 0.25);
-const vec3 sand = vec3(1, 1, 0.5);
-const vec3 white = vec3(1);
-const vec3 tanRock = sand;
-const vec3 pink = vec3(1, 0.8, 0.8);
-const vec3 colors[88] = vec3[](
-    green, grey, redRock, grey, lightGrey, soil, sand, sand, white, lightGrey, // 0-9
-    sand, soil, grey, grey, green, grey, white, white, white, redRock, // 10 - 19
-    lightGrey, orangeRock, grey, redRock, sand, soil, green, grey, sand, // 20 - 29
-    grey, grey, green, sand, grey, redRock, redRock, white, soil, sand, // 30 - 39
-    green, green, green, green, soil, grey, soil, grey, grey, grey, // 40 - 49
-    white, redRock, blueRock, sand, white, white, lightGrey, tanRock, redRock, green, // 50 - 59
-    sand, tanRock, tanRock, tanRock, sand, blueRock, white, grey, green, grey, // 60 - 69
-    green, redRock, sand, lightGrey, lightGrey, lightGrey, lightGrey, green, green, sand, // 70 - 79
-    sand, darkGreen, sand, soil, grey, green, green, pink, pink
-);
-
 void main() {
     vec4 material = texture(matTex, uv);
     int idx1 = int(material.x * 255);
