f006448b7a4b9a12a34a8817fe9672032f4da96f
torph <73564623+Torphedo@users.noreply.github.com>
2026-09-05 19:54:54 -0400

    Add initial BFRES parsing with radix tree walking This code is in a good enough state to find an FTEX by name.

---
+++ src/Bfres/ResourceFile.cs
@@ -0,0 +1,226 @@
+using System.Buffers.Binary;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Entish;
+using Entish.Attributes;
+using static Entish.EndianUtils;
+using OperationResult;
+using static OperationResult.Helpers;
+namespace terrainBench;
+
+public class ResourceFile {
+    public enum SubfileType {
+        FMDL = 0, /* Models */
+        FTEX = 1, /* Textures */
+        FSKA = 2, /* Skeletal animations */
+        FSHU_PARAMS = 3, /* Shader params */
+        FSHU_COLOR_ANIM = 4, /* Color animations */
+        FSHU_TEX_SRT = 5, /* Texture SRT animations */
+        FTXP = 6, /* Texture pattern animations */
+        FVIS_BONE = 7, /* Bone visibility animations */
+        FVIS_MATE = 8, /* Material visibility animations */
+        FSHA = 9, /* Shape animation (shape keys?) */
+        FSCN = 10, /* Scene animation */
+        EMBED = 11, /* Other embedded data */
+    };
+
+
+
+    // We have to do explicit layout to stop the compiler from adding padding.
+    [StructLayout(LayoutKind.Explicit, Size = 0x6C, Pack = 0)]
+    public unsafe struct ResFileHeader {
+        [FieldOffset(0x00)] public readonly uint magic;
+        [FieldOffset(0x04)] public readonly byte versionMajor;
+        [FieldOffset(0x05)] public readonly byte versionMinor;
+        [FieldOffset(0x06)] public readonly byte versionPatch;
+        [FieldOffset(0x07)] public readonly byte versionHotfix;
+        [FieldOffset(0x08)] public readonly ushort bom;
+        
+        [FieldOffset(0x0A)] public readonly ushort headerSize;
+        [FieldOffset(0x0C)] public readonly uint fileSize;
+        [FieldOffset(0x10)] public readonly uint fileAlignment;
+        [FieldOffset(0x14)] public readonly int filenameOffset;
+        [FieldOffset(0x18)] public readonly int stringTableLength;
+        [FieldOffset(0x1C)] public readonly int stringTableOffset;
+        [FieldOffset(0x20)] public fixed int subFileOffsets[12] ;
+        [FieldOffset(0x50)] public fixed ushort subFileCounts[12];
+        [FieldOffset(0x68)] public readonly uint userPointer;
+
+        public static unsafe void Swap(ResFileHeader* header) {
+            if (header->bom == 0xFEFF) {
+                return; // The data is in native endian
+            }
+
+            EndianUtils.Swap(&header->magic);
+            EndianUtils.Swap(&header->headerSize);
+            EndianUtils.Swap(&header->fileSize);
+            EndianUtils.Swap(&header->fileAlignment);
+            EndianUtils.Swap(&header->filenameOffset);
+            EndianUtils.Swap(&header->stringTableLength);
+            EndianUtils.Swap(&header->stringTableOffset);
+            for (uint i = 0; i < 12; i++)
+            {
+                EndianUtils.Swap(&header->subFileOffsets[i]);
+                EndianUtils.Swap(&header->subFileCounts[i]);
+            }
+            EndianUtils.Swap(&header->userPointer);
+        } 
+
+        public uint SubfileOffset(SubfileType t) {
+            uint i = (uint)t;
+            uint SUBFILE_OFFSET_TABLE_POS = 0x20;
+            uint offsetOfOffset = SUBFILE_OFFSET_TABLE_POS + (sizeof(uint) * i);
+            int offsetValue = subFileOffsets[i];
+            return (uint)(offsetOfOffset + offsetValue);
+        }
+    }
+    
+    
+    [StructLayout(LayoutKind.Sequential, Size = 0x10)]
+    readonly unsafe struct IndexGroupEntry {
+        public readonly uint searchValue;
+        public readonly ushort leftIndex;
+        public readonly ushort rightIndex;
+        public readonly int nameOffset;
+        public readonly int dataOffset;
+        
+        public static unsafe void Swap(ushort bom, IndexGroupEntry* e) {
+            if (bom == 0xFEFF) {
+                return; // The data is in native endian
+            }
+            EndianUtils.Swap(&e->searchValue);
+            EndianUtils.Swap(&e->leftIndex);
+            EndianUtils.Swap(&e->rightIndex);
+            EndianUtils.Swap(&e->nameOffset);
+            EndianUtils.Swap(&e->dataOffset);
+        } 
+    };
+    
+    [StructLayout(LayoutKind.Sequential, Size = 0x8)]
+    public readonly unsafe struct IndexGroup {
+        public readonly uint groupSize;
+        public readonly int entryCount;
+        
+        public static unsafe void Swap(ushort bom, IndexGroup* header) {
+            if (bom == 0xFEFF) {
+                return; // The data is in native endian
+            }
+            EndianUtils.Swap(&header->groupSize);
+            EndianUtils.Swap(&header->entryCount);
+        } 
+    };
+    
+    private static unsafe ref T ReadUnsafe<T>(ReadOnlySpan<byte> span, long offset)
+        where T : unmanaged
+    {
+        Debug.Assert(offset >= 0 && (offset + sizeof(T)) < span.Length);
+        fixed (byte* ptr = span) {
+            return ref *(T*)(ptr + offset);
+        }
+    }
+
+    private static unsafe ReadOnlySpan<T> SpanSegment<T>(ReadOnlySpan<byte> span, long offset, int count)
+        where T : unmanaged
+    {
+        Debug.Assert(offset >= 0 && count >= 0);
+        Debug.Assert((offset + sizeof(T) * count) < span.Length);
+        fixed (byte* ptr = span) {
+            var pos = (T*)(ptr + offset);
+            return new ReadOnlySpan<T>(pos, count);
+        }
+    }
+
+    public readonly struct SubfileHandle(SubfileType t, uint offset, uint count) {
+        public readonly SubfileType type = t;
+        public readonly uint indexGroupOffset = offset;
+        public readonly uint fileCount = count;
+    }
+    
+    public static Result<SubfileHandle, ErrorStack> GetSubfile(ReadOnlySpan<byte> data, SubfileType t) {
+        unsafe {
+            var header = ReadUnsafe<ResFileHeader>(data, 0);
+            ResFileHeader.Swap(&header);
+
+            var count = header.subFileCounts[(int)t];
+            var offset = header.SubfileOffset(t);
+
+            if (offset == 0) {
+                return Err(new ErrorStack($"The BFRES didn't contain a subfile of type {t}"));
+            }
+            return new SubfileHandle(t, offset, count);
+        }
+    }
+
+    public static uint FindSubfileEntry(ReadOnlySpan<byte> data, SubfileHandle h, string name) {
+        var header = ReadUnsafe<ResFileHeader>(data, 0);
+
+        unsafe {
+            var group = ReadUnsafe<IndexGroup>(data, h.indexGroupOffset);
+            IndexGroup.Swap(header.bom, &group);
+            uint entriesPos = (uint)(h.indexGroupOffset + sizeof(IndexGroup));
+            var entries = SpanSegment<IndexGroupEntry>(data, entriesPos, group.entryCount + 1);
+
+
+            // Walk the radix tree of entries. See:
+            // https://mk8.tockdom.com/wiki/BFRES_(File_Format)#Index_Group
+            // Also see BfresLibrary.ResDict.Traverse() in the BfresLibrary project:
+            // https://github.com/KillzXGaming/BfresLibrary/blob/master/BfresLibrary/Shared/Common/ResDict.cs#L515-L539
+            
+            IndexGroupEntry parent = entries[0];
+            IndexGroupEntry.Swap(header.bom, &parent);
+            
+            uint childIdx = parent.leftIndex;
+            IndexGroupEntry child = entries[(int)childIdx];
+            IndexGroupEntry.Swap(header.bom, &child);
+            
+            // Each entry points to a specific bit in a specific character of
+            // the filename. The value of that bit determines whether we walk
+            // left or right down the tree.
+            while (parent.searchValue > child.searchValue) {
+                parent = child;
+                
+                // Get the bit pointed to by this entry
+                uint charpos = child.searchValue >> 3;
+                byte bitpos = (byte)(child.searchValue & 0b111);
+                int direction = 0;
+                if (charpos < name.Length) {
+                    int c = name[(int)charpos] >> bitpos;
+                    direction = c & 1;
+                }
+                
+                // Walk down the tree based on the target bit
+                childIdx = direction == 1 ? child.rightIndex : child.leftIndex; 
+                child = entries[(int)childIdx];
+                IndexGroupEntry.Swap(header.bom, &child);
+                
+                if (childIdx == 0 || childIdx > group.entryCount) {
+                    Console.WriteLine("Can't find '{0}'", name);
+                    return 0; // Unable to find entry
+                }
+            }
+
+            var targetEntryPos = entriesPos + sizeof(IndexGroupEntry) * childIdx;
+            // The pointers are relative to the pointer values themselves, and
+            // C# doesn't seem to have anything like offsetof().
+            const int NAME_PTR_OFFSET_IN_ENTRY = 8;
+            const int DATA_PTR_OFFSET_IN_ENTRY = 12;
+            
+            var namePtr = targetEntryPos + NAME_PTR_OFFSET_IN_ENTRY + child.nameOffset;
+            var dataPtr = targetEntryPos + DATA_PTR_OFFSET_IN_ENTRY + child.dataOffset;
+            Console.WriteLine("Found '{0}' in entry {1}, name @ 0x{2:X}, data @ 0x{3:X}", name, childIdx, namePtr, dataPtr);
+            return (uint)dataPtr;
+        }
+    }
+    
+    public static void ParseBFRES(ReadOnlySpan<byte> data) {
+        uint pos = 0;
+        var ftexHandleRes = GetSubfile(data, SubfileType.FTEX);
+        if (ftexHandleRes.IsErr()) {
+            Console.WriteLine("Failed to find FTEX subfile!");
+        }
+
+        var ftexHandle = ftexHandleRes.Unwrap();
+        Console.WriteLine("FTEX offset 0x{0:x}, {1} entries", ftexHandle.indexGroupOffset, ftexHandle.fileCount);
+        FindSubfileEntry(data, ftexHandle, "MaterialAlb");
+    }
+}
